946a8be1fba6b78a9fdfe84783d609cceff49231
James Dowers <jdowers20@gmail.com>
2017-09-28 22:17:58 +1000

    Test files

---
+++ test4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3.6
+
+i = 0
+while (i < 4): print(i); i = i + 1
+
+while(i < 6):
+	i = i + 1
+	print(i)
+
+if (i == 6):
+	print(i);
+	i = i + 1 ; print(i)
+
+while i < 10:
+	i = i + 1;
+	print(i)
+
+while i < 15:
+	j = 0
+	while j < 5: print(j); j = j + 1
+	i = i + 1
+
+print(i)
---
+++ test5.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3.6
+
+i = 0
+j = 1
+
+rand = "test"
+if i == 4 and j == 2:
+	print("yew")
+
+if i==1 or j==1:
+	print(~i)
+
+if    (i> j or j >=i):
+	print(~i&j)
+
+i=~j
+
+j >> 2
+
+k=0
+if( k!=1):
+	print(i, j, "k")
+	print("kji", i, "j" ,k)
+
+
